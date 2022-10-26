516fbabc942cbd27e400a8478451455ca341b87c
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-26 22:51:49 +0800

    Added sections to transactions page

---
+++ src/components/sections/tracker/transactions/GraphSection.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="q-pa-md full-width bg-white rounded-borders">
+    <q-card class="full-width bg-grey-1" flat>
+      <q-card-section class="bg-green-1 rounded-borders">
+        <div class="row">
+          <div class="col-6 col-sm-6">
+            <q-avatar color="green-3">
+              <q-icon name="insights" />
+            </q-avatar>
+            Graph
+          </div>
+
+          <div class="col-6 col-sm-6 text-right">
+            <q-btn icon="add" color="primary" round unelevated outline />
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <p class="text-center text-body2 text-grey-8" v-for="n in 5" :key="n">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
+          perferendis totam, ea at omnis vel numquam exercitationem aut, natus
+          minima, porro labore.
+        </p>
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn-toggle
+          v-model="secondModel"
+          class="full-width my-custom-toggle"
+          spread
+          no-caps
+          unelevated
+          toggle-color="primary"
+          color="white"
+          text-color="primary"
+          :options="[
+            { label: 'Income', value: 'one' },
+            { label: 'Expenses', value: 'two' },
+          ]"
+        />
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "GraphSection",
+
+  setup() {
+    return {
+      secondModel: ref("one"),
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.my-custom-toggle {
+  border: 1px solid $primary;
+}
+</style>
--- src/pages/web/tracker/TransactionsPage.vue
+++ src/pages/web/tracker/TransactionsPage.vue
@@ -1,14 +1,30 @@
 <template>
   <q-page class="q-pa-md bg-grey-3 flex flex-center">
-    Transactions page
+    <div class="row wrap justify-left full-width q-gutter-md">
+      <span class="text-h5 text-weight-medium q-mb-md">Transactions</span>
+    </div>
+
+    <div class="row wrap justify-center full-width q-mb-md">
+      <graph-section />
+    </div>
+
+    <div class="row wrap justify-center full-width q-mb-md">
+      <lists-section />
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import GraphSection from "components/sections/tracker/transactions/GraphSection.vue";
+import ListsSection from "components/sections/tracker/transactions/ListsSection.vue";
 
 export default defineComponent({
   name: "TransactionsPage",
+  components: {
+    GraphSection,
+    ListsSection,
+  },
 });
 </script>
 
